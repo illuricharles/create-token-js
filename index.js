8df1a4fb1877e3d4325e8baeed5159f0770bb91a
Illuri charles <illuricharles6@gmail.com>
2024-09-08 16:55:02 +0530

    update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const {Connection, Keypair, clusterApiUrl, TOKEN_PROGRAM_ID, PublicKey} = require('@solana/web3.js')
 const {createMint, getOrCreateAssociatedTokenAccount, mintTo} = require('@solana/spl-token')
 
-const payer = Keypair.fromSecretKey(Uint8Array.from([135,205,106,46,161,193,182,234,102,97,228,7,76,146,213,95,136,145,228,110,68,92,174,199,12,232,191,7,164,4,241,49,214,120,97,220,4,215,72,208,32,163,57,25,157,255,209,113,136,136,34,72,26,80,142,171,8,123,39,116,199,173,168,131]))
+const payer = Keypair.fromSecretKey(Uint8Array.from("your-secret-key"))
 const mintAuthority = payer 
 const connection = new Connection(clusterApiUrl('devnet'))
 
